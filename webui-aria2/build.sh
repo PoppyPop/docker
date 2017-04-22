7225d327128e3d7e7e7a5c98a66ba1096d72744c
PoppyPop <skytep@gmail.com>
2017-04-23 00:15:25 +0200

    Correct tagging

--- webui-aria2/build.sh
+++ webui-aria2/build.sh
@@ -4,8 +4,9 @@
 git clone https://github.com/PoppyPop/aria2-alpine.git
 
 cd aria2-alpine
-sudo docker build -t poppypop/aria2-alpine --rm .
+sudo docker build -t poppypop/aria2:alpine --rm .
 cd -
+rm -rf aria2-alpine
 
 git clone https://github.com/ziahamza/webui-aria2.git
 
@@ -13,6 +14,8 @@ git clone https://github.com/ziahamza/webui-aria2.git
 cd webui-aria2
 sudo docker build -t poppypop/webui-aria2 --rm .
 cd -
+rm -rf webui-aria2
+
 
 sudo mkdir /srv/webui-aria2
 sudo mkdir /srv/webui-aria2/Downloads
