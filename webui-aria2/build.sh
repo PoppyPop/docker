c07b6b5902a7869da751e54767234c5e3e4b6641
PoppyPop <skytep@gmail.com>
2017-04-22 23:54:57 +0200

    Complete webui-aria2

--- webui-aria2/build.sh
+++ webui-aria2/build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
 
-git clone https://github.com/ziahamza/webui-aria2.git
-cd webui-aria2
+git clone https://github.com/PoppyPop/aria2-alpine.git
 
-docker build -t poppypop/webui-aria2 .
-
+cd aria2-alpine
+sudo docker build -t poppypop/aria2-alpine .
 cd -
+
+git clone https://github.com/ziahamza/webui-aria2.git
+
+
+cd webui-aria2
+sudo docker build -t poppypop/webui-aria2 .
+cd -
+
+sudo mkdir /srv/webui-aria2
+sudo mkdir /srv/webui-aria2/Downloads
