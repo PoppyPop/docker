effe4eb34940d986b1df1d7e4bebe671ab30e0bb
PoppyPop <skytep@gmail.com>
2017-05-06 01:40:03 +0200

    Better backup efk

--- backup-scripts/elastic/backup-elastic.sh
+++ backup-scripts/elastic/backup-elastic.sh
@@ -5,3 +5,18 @@ src=${BASH_SOURCE%/*}
 
 curator --config ${src}/config.yml ${src}/backup.yml
 
+backupdir=/srv/backs/efk
+backupsrc=/srv/datas/efk
+now=`date +"%Y-%m-%d_%H-%M"`
+
+# Test Folder
+if [ ! -d "$backupdir" ]; then
+  mkdir -p $backupdir
+fi
+
+# First: Remove old backups
+find $backupdir/ -type f -mtime +30 -delete
+
+# Add New Backup
+tar -zcf $backupdir/$now.tar.gz -C $backupsrc .
+
