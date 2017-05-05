4ce45c3df2ea64c8b66d7139f9d0532b91699c54
PoppyPop <skytep@gmail.com>
2017-05-05 22:13:44 +0200

    aria2

--- webui-aria2/init.sh
+++ webui-aria2/init.sh
@@ -8,8 +8,13 @@ sudo mkdir -p /srv/aria2/Extract
 sudo mkdir -p /srv/aria2/Ended
 sudo mkdir -p /srv/aria2/webui-aria2
 
-sudo cp ./ /srv/aria2/
-sudo cp ./ /srv/aria2/
+sudo cp -f autounrar.py /srv/aria2/
+sudo cp -f copyFile.sh /srv/aria2/
+sudo cp -f complete.sh /srv/aria2/Downloads/
+
+#conf
+sudo mkdir -p /srv/confs/aria2/
+sudo cp -f docker-compose.yml /srv/confs/aria2/
 
 sudo useradd -m download
 sudo chown -R download:download /srv/aria2/
