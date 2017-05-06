2759ed96fabbefbd7180edbbf552adfa53ab0e4b
PoppyPop <skytep@gmail.com>
2017-05-06 15:59:28 +0200

    bind

--- bind/init.sh
+++ bind/init.sh
@@ -5,4 +5,8 @@ sudo mkdir -p /srv/confs/bind
 
 sudo cp -f docker-compose.yml /srv/confs/bind/
 
-sudo cp -f conf/* /srv/confs/bind/
+sudo mkdir -p /srv/confs/bind/bind/etc/
+
+sudo cp -f conf/* /srv/confs/bind/bind/etc/
+
+sudo chown -R 102:105 /srv/confs/bind/bind/
