6b7c29586e955f3ef453a87d72aec21023d02c92
PoppyPop <skytep@gmail.com>
2017-10-08 23:51:02 +0200

    LDAP SSL

--- ldap/openldap/run.sh
+++ ldap/openldap/run.sh
@@ -3,4 +3,4 @@
 
 # docker run -ti --rm -v /srv/docker/dchpd:/data --log-driver=fluentd --log-opt fluentd-address=127.0.0.1:24224 --log-opt fluentd-async-connect --name ldap poppypop/ldap
 
-docker run -ti --rm -p 389:389 -p 636:636 --name ldap poppypop/ldap $1
+docker run -ti --rm -v /srv/datas/ldap/openldap:/var/lib/openldap/openldap-data -v /etc/ssl/private:/etc/ssl/private:ro -p 389:389 -p 636:636 --name ldap poppypop/ldap $1
