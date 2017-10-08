2398f7a44c1f166cbb0f68cbe8def16c91505dc7
PoppyPop <skytep@gmail.com>
2017-10-08 23:51:26 +0200

    PKI correct

--- pki/utils/sign-server.sh
+++ pki/utils/sign-server.sh
@@ -9,5 +9,6 @@ mkdir -p pki/publish/server/$1
 
 cp pki/sign/issued/$1.crt pki/publish/server/$1/
 cp pki/sign/private/$1.key pki/publish/server/$1/
+cp pki/sign/ca.crt pki/publish/server/$1/
 
-openssl pkcs12 -export -out pki/publish/server/$1/$1.p12 -inkey pki/publish/server/$1/$1.key -in pki/publish/server/$1/$1.crt -certfile pki/publish/ca.crt -passout pass:
+openssl pkcs12 -export -out pki/publish/server/$1/$1.p12 -inkey pki/publish/server/$1/$1.key -in pki/publish/server/$1/$1.crt -certfile pki/publish/server/$1/ca.crt -passout pass:
