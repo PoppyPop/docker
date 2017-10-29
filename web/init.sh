180dc2c2c6eaa1f202f5472e8a73fe47a7f277e0
PoppyPop <skytep@gmail.com>
2017-10-29 23:20:49 +0100

    Net Perf

--- web/init.sh
+++ web/init.sh
@@ -20,6 +20,6 @@ sudo cp -f nginx/conf/conf.d/* /srv/confs/web/conf.d/
 sudo cp -f varnish/default.vcl /srv/confs/web/
 
 #docker network
-sudo docker network create webbackend
+sudo docker network create --opt com.docker.network.driver.mtu=9000 webbackend
 
 
