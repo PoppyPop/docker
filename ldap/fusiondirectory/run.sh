d7dd4352c86be32dce4400c94c44ae68ff61a30f
PoppyPop <skytep@gmail.com>
2017-11-27 22:51:01 +0100

    LDAP Make

--- ldap/fusiondirectory/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-
-
-# docker run -ti --rm -v /srv/datas/ldap/fusiond/fusiondirectory.conf:/etc/fusiondirectory/fusiondirectory.conf --name fusiond -p 12080:80 poppypop/fusiond $1
-
-docker run -ti --rm --name fusiond -p 12080:80 poppypop/fusiond $1
