42998a7cd610563b963271ba085eb52cab7cc1be
PoppyPop <skytep@gmail.com>
2017-12-17 01:10:22 +0100

    PDNS almost done

--- pdns/init.sh
+++ pdns/init.sh
@@ -8,10 +8,12 @@ sudo cp -f docker-compose.yml /srv/confs/pdns/
 sudo cp -f db.env /srv/confs/pdns/
 sudo cp -f pdns-admin.env /srv/confs/pdns/
 sudo cp -f pdns.env /srv/confs/pdns/
+sudo cp -f pdns-api.env /srv/confs/pdns/
 
 #datas
 docker volume create pdns-mysql --label backup=yes
 docker volume create pdns-admin-upload --label backup=yes
+docker volume create pdns-recursor-datas --label backup=yes
 
 #docker network
 #sudo docker network create --opt com.docker.network.driver.mtu=9000 ldap-backend
