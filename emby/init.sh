76b943c83dea339d5a8d4f243e0bcf50ea85307b
Steve HOLWEG <skytep@gmail.com>
2018-01-07 11:49:49 -0800

    Emby ADD

---
+++ emby/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+#confs
+sudo mkdir -p /srv/confs/emby
+
+sudo cp -f docker-compose.yml /srv/confs/emby/
+
+#datas
+docker volume create emby-datas --label backup=yes
+
+#backs
+sudo mkdir -p /srv/backs/pdns
