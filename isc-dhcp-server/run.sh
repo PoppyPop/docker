b1ada36308119b58904911c65bb4aa3a70646cbe
Steve HOLWEG <skytep@gmail.com>
2018-03-25 17:35:30 +0200

    Delete obsolete

--- isc-dhcp-server/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-
-docker run -ti --rm --net=host -v /srv/docker/dchpd:/data --log-driver=fluentd --log-opt fluentd-address=127.0.0.1:24224 --log-opt fluentd-async-connect --name mydhcpd poppypop/dhcpd
