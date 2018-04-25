d975363966fbb125e0d85d79cb1f93e047fdfc6d
PoppyPop <skytep@gmail.com>
2018-04-25 21:41:35 +0200

    Emby definitive edition

--- emby/autoorganize.sh
+++ emby/autoorganize.sh
@@ -7,3 +7,6 @@ set -e
 docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
 	cp -f /local/Emby.AutoOrganize.dll /dist/plugins/Emby.AutoOrganize.dll
 	
+	docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
+	cp -f /local/Emby.addic7ed.dll /dist/plugins/Emby.addic7ed.dll
+	
