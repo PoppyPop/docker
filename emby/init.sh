4c31a99da0ae0ecf2ab28872c77d6d3af2d29fa7
Steve HOLWEG <skytep@gmail.com>
2018-04-27 09:30:10 +0200

    Emby init change

--- emby/init.sh
+++ emby/init.sh
@@ -9,5 +9,7 @@ sudo cp -f docker-compose.yml /srv/confs/emby/
 #datas
 docker volume create emby-datas --label backup=yes
 
+docker volume create -o type=none -o device=/datas -o o=bind emby-local-datas
+
 #backs
 sudo mkdir -p /srv/backs/emby
