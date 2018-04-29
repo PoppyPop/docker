4c85db4dca465cd7a0c43cd84b71d7ff49ad7691
Steve HOLWEG <skytep@gmail.com>
2018-04-29 22:29:46 +0200

    Nextcloud update

--- nextcloud/init.sh
+++ nextcloud/init.sh
@@ -7,7 +7,11 @@ sudo mkdir -p /srv/confs/nextcloud
 sudo cp -f docker-compose.yml /srv/confs/nextcloud/
 
 #datas
-docker volume create nextcloud-data --label backup=yes
+docker volume create nextcloud-base --label backup=yes
+docker volume create nextcloud-apps --label backup=yes
+docker volume create nextcloud-config --label backup=yes
+
+docker volume create -o type=none -o device=/datas/nextcloud -o o=bind nextcloud-data
 
 #backs
 #sudo mkdir -p /srv/backs/ldap
