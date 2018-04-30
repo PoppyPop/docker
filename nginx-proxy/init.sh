537fe4ab5ebe7df2e9d09fee5ca3ff7ffaa38b63
PoppyPop <skytep@gmail.com>
2018-04-30 10:35:00 +0200

    Security upgrade

--- nginx-proxy/init.sh
+++ nginx-proxy/init.sh
@@ -7,5 +7,9 @@ sudo docker network create --opt com.docker.network.driver.mtu=9000 proxy-net
 sudo mkdir -p /srv/confs/nginx-proxy/
 
 sudo cp -f docker-compose.yml /srv/confs/nginx-proxy/
-sudo curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl -o /srv/confs/nginx-proxy/nginx.tmpl
+
+sudo cp -f nginx.tmpl /srv/confs/nginx-proxy/nginx.tmpl
+#sudo curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl -o /srv/confs/nginx-proxy/nginx.tmpl
+
+
 
