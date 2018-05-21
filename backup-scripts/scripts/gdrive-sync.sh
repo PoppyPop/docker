bb2336b1658b5e7b227be576a5e452a25563e176
PoppyPop <skytep@gmail.com>
2018-05-21 10:29:12 +0200

    Rclone update

--- backup-scripts/scripts/gdrive-sync.sh
+++ backup-scripts/scripts/gdrive-sync.sh
@@ -4,7 +4,7 @@
 # delete server file older than 7days
 #sudo rclone --min-age 7d delete gdsecret:$(hostname -f)
 
-sudo rclone sync /srv/backs/ gdsecret:$(hostname -f)
+sudo rclone sync --drive-use-trash=false /srv/backs/ gdsecret:$(hostname -f)
 
 if [ $? -eq 0 ] 
 then
