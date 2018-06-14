43e0c38bd0602ee0479db35475b842d3a3c6fd4e
PoppyPop <skytep@gmail.com>
2018-06-14 17:05:42 +0200

    Convert to pgsql

--- pdns/init.sh
+++ pdns/init.sh
@@ -8,8 +8,9 @@ sudo cp -fr conf/ /srv/confs/pdns/
 sudo cp -f docker-compose.yml /srv/confs/pdns/
 
 #datas
-docker volume create pdns-mysql --label backup=yes
+#docker volume create pdns-mysql --label backup=yes
 docker volume create pdns-admin-upload --label backup=yes
+docker volume create pdns-pgsql --label backup=yes
 
 #docker network
 #sudo docker network create --opt com.docker.network.driver.mtu=9000 ldap-backend
