60a95af53da2ccbc6f2421540de4bc7212ec6af6
PoppyPop <skytep@gmail.com>
2018-06-17 10:44:18 +0200

    Emby update tune

--- emby/autoorganize.sh
+++ emby/autoorganize.sh
@@ -6,14 +6,14 @@ set -e
 
 AUTOORGURL=$(curl -L -s -u "PoppyPop" https://api.github.com/repos/PoppyPop/Emby.AutoOrganize/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.AutoOrganize.dll\")) | .browser_download_url")
 
-curl -s $AUTOORGURL > Emby.AutoOrganize.dll
+curl -L -s $AUTOORGURL > Emby.AutoOrganize.dll
 
 docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
 	cp -f /local/Emby.AutoOrganize.dll /dist/plugins/Emby.AutoOrganize.dll
 	
 AUTOORGURL=$(curl -L -s -u "PoppyPop" https://api.github.com/repos/PoppyPop/Emby.addic7ed/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.addic7ed.dll\")) | .browser_download_url")
 
-curl -s $AUTOORGURL > Emby.addic7ed.dll	
+curl -L -s $AUTOORGURL > Emby.addic7ed.dll	
 
 docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
 	cp -f /local/Emby.addic7ed.dll /dist/plugins/Emby.addic7ed.dll
--- emby/update.sh
+++ emby/update.sh
@@ -4,3 +4,5 @@
 docker-compose pull
 
 ./autoorganize.sh
+
+sudo systemctl restart docker-compose@emby
