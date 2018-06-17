fcf4cfadf7ed851d2d5a0ff4758ffcc7fe3381ae
PoppyPop <skytep@gmail.com>
2018-06-17 10:36:26 +0200

    Emby dll update

--- emby/autoorganize.sh
+++ emby/autoorganize.sh
@@ -4,9 +4,17 @@
 # Fail on error
 set -e
 
+AUTOORGURL=$(curl -L -s -u "PoppyPop" https://api.github.com/repos/PoppyPop/Emby.AutoOrganize/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.AutoOrganize.dll\")) | .browser_download_url")
+
+curl -s $AUTOORGURL > Emby.AutoOrganize.dll
+
 docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
 	cp -f /local/Emby.AutoOrganize.dll /dist/plugins/Emby.AutoOrganize.dll
 	
-	docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
+AUTOORGURL=$(curl -L -s -u "PoppyPop" https://api.github.com/repos/PoppyPop/Emby.addic7ed/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.addic7ed.dll\")) | .browser_download_url")
+
+curl -s $AUTOORGURL > Emby.addic7ed.dll	
+
+docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
 	cp -f /local/Emby.addic7ed.dll /dist/plugins/Emby.addic7ed.dll
 	
