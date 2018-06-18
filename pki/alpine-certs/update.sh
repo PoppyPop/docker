a94e861681de8ed0e991b6e23095f366633e0048
PoppyPop <skytep@gmail.com>
2018-06-18 16:22:33 +0200

    Update alpine ca certs

--- pki/alpine-certs/update.sh
+++ pki/alpine-certs/update.sh
@@ -5,10 +5,12 @@ set -x
 
 apk add --update ca-certificates curl
 
+rm -rf /etc/ssl/certs/*
+
 #alpine
 mkdir -p /usr/local/share/ca-certificates/
-curl -X GET http://yugo.moot.fr:8887/int.pem | tee /usr/local/share/ca-certificates/int.pem  > /dev/null
-curl -X GET http://yugo.moot.fr:8887/ca.pem | tee /usr/local/share/ca-certificates/ca.pem  > /dev/null
+curl -X GET http://yugo.moot.fr:8887/int.pem | tee /usr/local/share/ca-certificates/Moot.fr_INT.pem  > /dev/null
+curl -X GET http://yugo.moot.fr:8887/ca.pem | tee /usr/local/share/ca-certificates/Moot.fr_CA.pem  > /dev/null
 
 update-ca-certificates
 
