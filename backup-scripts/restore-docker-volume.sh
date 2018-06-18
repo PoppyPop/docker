319781de58f3ad2e02f3dc5d684f2e0f28605c46
PoppyPop <skytep@gmail.com>
2018-06-18 20:34:32 +0200

    Scripts Up

--- backup-scripts/restore-docker-volume.sh
+++ backup-scripts/restore-docker-volume.sh
@@ -4,5 +4,5 @@
 #if [ -f "/srv/backs/$1"
 
 
-docker run -it -v $1:/volume -v /srv/backs:/srv/backs alpine \
+docker run -it --rm -v $1:/volume -v /srv/backs:/srv/backs alpine \
     sh -c "rm -rf /volume/* ; tar -C /volume/ -xjf $2"
