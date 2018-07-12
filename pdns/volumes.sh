6dc37e37a31cb5a65e005e1bf160b95934e0d457
PoppyPop <skytep@gmail.com>
2018-07-12 10:37:10 +0200

    Backup pdns

--- pdns/volumes.sh
+++ pdns/volumes.sh
@@ -5,5 +5,6 @@
 #docker volume create pdns-mysql --label backup=yes
 docker volume create pdns-admin-upload --label backup=yes
 docker volume create pdns-pgsql --label backup=yes
+docker volume create pdns-admin-db --label backup=yes
 
 docker volume create pdns-lua-conf
