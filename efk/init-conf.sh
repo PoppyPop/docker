42c342332d9a7511e11c447f82b10da7ddd3eeff
PoppyPop <skytep@gmail.com>
2019-05-26 22:20:03 +0200

    Passage amazon elastic

--- efk/init-conf.sh
+++ efk/init-conf.sh
@@ -2,4 +2,4 @@
 #
 
 docker run -it --rm -v fluentd-conf:/volume -v ${PWD}/fluentd:/orig alpine \
-    sh -c "rm -rf /volume/* ; cp -fa /orig/fluentd.conf /volume/ ; cp -fa /orig/docker-log.tmpl /volume/ ; touch /volume/docker-log.conf"
+    sh -c "rm -rf /volume/* ; cp -fa /orig/fluent.conf /volume/ ; cp -fa /orig/docker-log.tmpl /volume/ ; touch /volume/docker-log.conf"
