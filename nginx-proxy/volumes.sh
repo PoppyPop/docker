d8e1cede69237c98b7f50e5c8b35d40d58b2cfa0
PoppyPop <skytep@gmail.com>
2019-07-07 23:26:29 +0200

    Add certbot dual run

--- nginx-proxy/volumes.sh
+++ nginx-proxy/volumes.sh
@@ -4,6 +4,8 @@
 #datas
 docker volume create certs --label backup=yes
 
+docker volume create letsencrypt --label backup=yes
+
 docker volume create ngproxy-tmpl
 
 docker volume create ngproxy-vhost
