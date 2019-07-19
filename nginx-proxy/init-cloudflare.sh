f262df9fa7a4fda1451a985d8023c0ffb13f3216
PoppyPop <skytep@gmail.com>
2019-07-19 15:11:24 +0200

    Correct init

--- nginx-proxy/init-cloudflare.sh
+++ nginx-proxy/init-cloudflare.sh
@@ -5,5 +5,5 @@ read -p "Cloudflare user: " CFUSER
 read -p "Cloudflare password: " CFPASSWORD
 
 docker run -it --rm -v letsencrypt:/volume alpine \
-    sh -c "rm -rf /volume/cloudflare.ini ; printf 'dns_cloudflare_email=${CFUSER}\ndns_cloudflare_api_key=${CFPASSWORD}\n' > /volume/cloudflare.ini ; chmod a-x,go-rw /volume/cloudflare.ini "
+    sh -c "rm -rf /volume/cloudflare.ini ; printf 'dns_cloudflare_email=${CFUSER}\ndns_cloudflare_api_key=${CFPASSWORD}\n' > /volume/cloudflare.ini ; chmod a-x,go-rw /volume/cloudflare.ini ; mkdir -p /volume/links "
 	
