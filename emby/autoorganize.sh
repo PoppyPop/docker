1bc845f2c85d0bac440957b7e9921e7d6a64803a
PoppyPop <skytep@gmail.com>
2019-08-11 15:37:02 +0200

    Updates

--- emby/autoorganize.sh
+++ emby/autoorganize.sh
@@ -4,19 +4,25 @@
 # Fail on error
 set -e
 
-AUTOORGURL=$(curl -L -s https://api.github.com/repos/PoppyPop/Emby.AutoOrganize/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.AutoOrganize.dll\")) | .browser_download_url")
+#AUTOORGURL=$(curl -L -s https://api.github.com/repos/PoppyPop/Emby.AutoOrganize/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.AutoOrganize.dll\")) | .browser_download_url")
 
-curl -L -s $AUTOORGURL > Emby.AutoOrganize.dll
+#curl -L -s $AUTOORGURL > Emby.AutoOrganize.dll
+
+#docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
+#	sh -c 'mkdir -p /dist/plugins/ && cp -f /local/Emby.AutoOrganize.dll /dist/plugins/Emby.AutoOrganize.dll'
+
+#docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
+#        sh -c 'rm -rf /dist/plugins/Emby.AutoOrganize.dll'
+
+#AUTOORGURL=$(curl -L -s https://api.github.com/repos/PoppyPop/Emby.addic7ed/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.addic7ed.dll\")) | .browser_download_url")
+
+#curl -L -s $AUTOORGURL > Emby.addic7ed.dll
+
+#docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
+#	sh -c 'mkdir -p /dist/plugins/ && cp -f /local/Emby.addic7ed.dll /dist/plugins/Emby.addic7ed.dll'
+
+#rm Emby.addic7ed.dll Emby.AutoOrganize.dll
 
 docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
-	sh -c 'mkdir -p /dist/plugins/ && cp -f /local/Emby.AutoOrganize.dll /dist/plugins/Emby.AutoOrganize.dll'
-	
-AUTOORGURL=$(curl -L -s https://api.github.com/repos/PoppyPop/Emby.addic7ed/releases/latest | jq -r ".assets[] | select(.name | test(\"Emby.addic7ed.dll\")) | .browser_download_url")
+	sh -c 'mkdir -p /dist/plugins/ && rm -f /local/Emby.addic7ed.dll /dist/plugins/Emby.addic7ed.dll'
 
-curl -L -s $AUTOORGURL > Emby.addic7ed.dll	
-
-docker run --rm  -v emby-datas:/dist -v ${PWD}:/local alpine \
-	sh -c 'mkdir -p /dist/plugins/ && cp -f /local/Emby.addic7ed.dll /dist/plugins/Emby.addic7ed.dll'
-	
-rm Emby.addic7ed.dll Emby.AutoOrganize.dll
-	
---
+++ nextcloud/update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+
+docker-compose pull
+
+sudo service docker-compose@nextcloud restart
