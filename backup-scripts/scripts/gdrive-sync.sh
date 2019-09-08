1e382b96bca7dd9d1bb644bcccdc5bb358b5075e
PoppyPop <skytep@gmail.com>
2019-09-08 15:26:40 +0200

    Reuse trash

--- backup-scripts/scripts/gdrive-sync.sh
+++ backup-scripts/scripts/gdrive-sync.sh
@@ -4,7 +4,9 @@
 # delete server file older than 7days
 #sudo rclone --min-age 7d delete gdsecret:$(hostname -f)
 
-sudo rclone sync --drive-use-trash=false /srv/backs/ gdsecret:$(hostname -f)
+# --drive-use-trash=false
+
+sudo rclone sync /srv/backs/ gdsecret:$(hostname -f)
 
 if [ $? -eq 0 ] 
 then
