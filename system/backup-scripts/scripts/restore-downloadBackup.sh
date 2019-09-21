8933ca9b1a880344da1dbd192c2da7fcf9cd033e
PoppyPop <skytep@gmail.com>
2019-09-21 10:30:36 +0000

    Optim restore

--- system/backup-scripts/scripts/restore-downloadBackup.sh
+++ system/backup-scripts/scripts/restore-downloadBackup.sh
@@ -7,8 +7,20 @@ backupDir=$1
 
 # Restore Baskup file
 if [ ! -d "$backupDir" ]; then
+
+	result=""
+
+	until [ "$result" == "h" ] || [ "$result" == "d" ]; do
+		read -p "1hr (h) or 1Day (d) ? (Ctrl-C to stop)" result
+	done
+
+	maxage="1d"
+	if [ "$result" == "h" ]; then
+		maxage="1h"
+	fi
+
 	mkdir "$backupDir"
-	rclone copy --max-age 1d  gdsecret:/$(hostname -f)/daily/ "$backupDir"
+	rclone copy --max-age $maxage  gdsecret:/$(hostname -f)/daily/ "$backupDir"
 else
 	echo "Directory already exist, restore already in progress"
 fi
