b6d05a74e5549bf4e2133197854d193347bef4a2
PoppyPop <skytep@gmail.com>
2019-09-21 07:12:41 +0000

    Reorganize for better restore

---
+++ backup-scripts/scripts/gitconf/restore-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+
+# Restore volume
+find /opt/poppypop/docker/ -name 'volumes.sh' -exec sh {} \;
+
+# Restore network
+find /opt/poppypop/docker/ -name 'networks.sh' -exec sh {} \;
+
+# Restore init
+find /opt/poppypop/docker/ -name 'init.sh' -exec sh {} \;
+
+# Restore Conf
+find /opt/poppypop/docker/ -name 'init-*.sh' -exec sh {} \;
